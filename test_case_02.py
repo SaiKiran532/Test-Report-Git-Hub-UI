7a1d2bea0cc506a97fb11057aa6912ecf9dddcf6
Saikiran532 <saikiranreddychalla532@gmail.com>
2024-05-21 11:29:35 +0530

    Test report

---
+++ test_case_02.py
@@ -0,0 +1,21 @@
+def test_method5():
+    x=10
+    y=11
+    assert x==y
+
+def test_method6():
+    a=2
+    b=3
+    assert a + b == 5
+
+def test_method7():
+    x=10
+    y=101
+    assert x==y
+
+
+
+def test_method8():
+    a=2
+    b=2
+    assert a + b == 4
