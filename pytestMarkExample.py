460427c8e99ff75d6d13d6a602dc238d46bbc7f5
Saikiran532 <saikiranreddychalla532@gmail.com>
2024-05-21 07:47:01 +0530

    Test report test

---
+++ pytestMarkExample.py
@@ -0,0 +1,27 @@
+import pytest
+
+
+@pytest.mark.one
+def test_method1():
+    x=10
+    y=20
+    assert x==y
+
+
+@pytest.mark.two
+def test_method2():
+    a=2
+    b=3
+    assert a + b == 5
+
+def test_method3():
+    x=10
+    y=10
+    assert x==y
+
+
+@pytest.mark.two
+def test_method4():
+    a=2
+    b=2
+    assert a + b == 5
