c16a7a026ede56b8bb2685a2278cf8edf21e1144
Saikiran532 <saikiranreddychalla532@gmail.com>
2024-05-24 11:04:13 +0530

    test-006

--- tests/Pyexmp.py
+++ tests/Pyexmp.py
@@ -1,21 +1,21 @@
-def test_method1():
+def test_method6():
     x=10
     y=10
     assert x==y
 
-def test_method2():
+def test_method7():
     a=2
     b=3
     assert a + b == 5
 
-def test_method3():
+def test_method8():
     x=12
     y=12
     assert x==y
 
 
 
-def test_method4():
+def test_method9():
     a=2
     b=2
     assert a + b == 4
